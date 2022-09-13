25837ead2473b1cc97c4d7fbe5d2677d6e560d6e
1694871021 <34471441+1694871021@users.noreply.github.com>
2022-09-13 20:09:34 +0800

    Add files via upload
    初始化

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# blog
-个人博客
-11111
+
+git上传远程仓库
+
+1.git init首先新建一个文件夹在当前文件夹下初始化一个git仓库
+
+2.git add . （将当前工作区的所有文件存放到暂存区）
+
+3.git commit -m “我的第一提交”
+
+4.git remote add origin 远程仓库地址
+
+5.git push -u origin master
+
+webpack工作原理
+
+Webpack 启动后，会根据我们的配置，找到项目中的某个指定文件（一般这个文件都会是一个 JS 文件）作为入口。然后顺着入口文件中的代码，根据代码中出现的 import（ES Modules）或者是 
+require（CommonJS）之类的语句，解析推断出来这个文件所依赖的资源模块，然后再分别去解析每个资源模块的依赖，周而复始，最后形成整个项目中所有用到的文件之间的依赖关系树
+
+有了这个依赖关系树过后， Webpack 会遍历（递归）这个依赖树，找到每个节点对应的资源文件，然后根据配置选项中的 Loader 配置，交给对应的 Loader 去加载这个模块，最后将加载的结果放入 bundle.js
+（打包结果）中，从而实现整个项目的打包
+
+  一、Webpack CLI 启动打包流程；
+  二、载入 Webpack 核心模块，创建 Compiler 对象；
+  三、使用 Compiler 对象开始编译整个项目；
+  四、从入口文件开始，解析模块依赖，形成依赖关系树；
+  五、递归依赖树，将每个模块交给对应的 Loader 处理；
+  六、合并 Loader 处理完的结果，将打包结果输出到 dist 目录。
+
+项目启动:
+  npm run dev
+项目打包:
+  npm run build
+
+!!! 最后在引入npm包时候一定要注意包的版本，某些包版本不一致就会一直报错，找不到原因
+
+引入vue2 npm i vue@2 vue-loader@15 vue-template-compiler@2
+引入vue3 npm i vue@3 vue-loader@16 vue-template-compiler@3
+
+122133
+
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+var { resolve } = require('path');
+var VueLoaderPlugin = require('vue-loader/lib/plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/main.js', //webpack打包入口
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: './src/'  // webpack服务器访问入口
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+        // options: {
+        //   loaders: {
+        //     css: ExtractTextPlugin.extract({
+        //       use: 'css-loader',
+        //       fallback: 'vue-style-loader'
+        //     })
+        //   }
+        // }
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          // 'style-loader',
+          'css-loader'
+        ]
+      },
+      // 处理图片的loader
+      {
+        test: /\.(pgn|jpg|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          // 关闭es6模块
+          esModule: false
+        },
+      },
+      // 处理html中的图片的loader
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        // outputPath: ''指定模块输目录
+      },
+      // // 处理其他资源的loader
+      // {
+      //   exclude: /\.(html|js|css|less|jpg|png|gif)/,
+      //   loader: 'file-loader',
+      //   options: {
+      //     name: '[hash:10].[ext]'
+      //   }
+      // }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'vue-webpack-te',
+      template: './src/public/index.html',
+      filename: 'index.html' //输出的文件
+    })
+  ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  // devServer: {
+  //   open: false,
+  //   host: "127.0.0.1",
+  //   port: 3082,
+  //   proxy: {
+  //     "/apis": {
+  //         target: "http://192.168.0.224:18090", // 接口域名
+  //         ws: true,
+  //         changeOrigin: true // 是否跨域
+  //     }
+  //   }
+  // }
+  mode: 'development', // 开发模式
+}
